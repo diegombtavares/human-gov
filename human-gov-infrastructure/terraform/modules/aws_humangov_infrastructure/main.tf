1dfb83c5fbff9512b750d88132e62d3b4f69628e
Diego Tavares <diegotavares.infra@gmail.com>
2024-12-10 13:22:22 -0300

    reorganização dos arquivos

--- human-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
+++ human-gov-infrastructure/terraform/modules/aws_humangov_infrastructure/main.tf
@@ -61,3 +61,49 @@ resource "aws_s3_bucket" "state_s3" {
     Name = "humangov-${var.state_name}"
   }
 }
+
+resource "aws_iam_role" "s3_dynamodb_full_access_role" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+        Name = "humangov-${var.state_name}"
+  }  
+  
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+
+}
+
+resource "aws_iam_instance_profile" "s3_dynamodb_full_access_instance_profile" {
+  name = "humangov-${var.state_name}-s3_dynamodb_full_access_instance_profile"
+  role = aws_iam_role.s3_dynamodb_full_access_role.name
+
+  tags = {
+      Name = "humangov-${var.state_name}"
+  }  
+}
